984fb904b4112bd75f83134ffb27c9dc5a67909b
Arman Bilge <armanbilge@gmail.com>
2022-10-19 16:28:37 +0000

    Setup flake, .jvmopts

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    typelevel-nix.url = "github:typelevel/typelevel-nix";
+    nixpkgs.follows = "typelevel-nix/nixpkgs";
+    flake-utils.follows = "typelevel-nix/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, typelevel-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ typelevel-nix.overlay ];
+        };
+      in
+      {
+        devShell = pkgs.devshell.mkShell {
+          imports = [ typelevel-nix.typelevelShell ];
+          name = "fs2-dom-shell";
+          typelevelShell = {
+            jdk.package = pkgs.jdk17;
+            nodejs.enable = true;
+          };
+          packages = [ pkgs.chromium pkgs.chromedriver pkgs.firefox pkgs.geckodriver ];
+        };
+      }
+    );
+}
